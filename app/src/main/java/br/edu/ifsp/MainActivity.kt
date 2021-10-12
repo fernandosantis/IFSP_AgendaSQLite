8ba2128f1b97ddf70eab76e8bbe4efb9d775de8b
Fernando Santis <fpsantis@gmail.com>
2021-10-12 12:59:41 -0300

    Aula 1 - Contato e DatabaseHelper

--- app/src/main/java/br/edu/ifsp/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/MainActivity.kt
@@ -10,5 +10,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+/*        // Testando DatabaseHelper
+
+        val db = DatabaseHelper(this)
+
+        val c1 = Contato(1,"Fernando", 123, "fpsantis@gmail.com")
+        val c2 = Contato(2,"Josi", 321, "pajojane@gmail.com")
+
+        db.inserirContato(c1)
+        db.inserirContato(c2)
+
+        val cursor = db.listarContatos()
+        cursor.forEach {
+            //
+        }*/
+
+
+
     }
 }
